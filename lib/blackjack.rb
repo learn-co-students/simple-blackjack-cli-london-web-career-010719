3fbdb9696857c1aa154d8ce0371e9c5218773e82
parik panchal <parikspanchal@icloud.com>
2018-12-27 10:14:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,10 +56,12 @@ end
 #####################################################
 
 def runner
-  welcome()
-  initial_round()
-  until initial_round + hit? <= 21
-    hit?
-  else
+  welcome
+  card_total = initial_round
+  until card_total > 21 do
+    card_total = hit?(card_total)
+    display_card_total(card_total)
   end
+  end_game(card_total)
+ end
 end
